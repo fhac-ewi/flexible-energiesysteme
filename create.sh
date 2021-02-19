5a4f2f30d9ca8820abe6f186b64cc8fdafe323a4
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-02-19 10:10:58 +0100

    fixed link

--- create.sh
+++ create.sh
@@ -14,7 +14,7 @@ do
     find "$D" -name "*.md" -maxdepth 1 -type f | while read -r F
     do
         echo "Appending file $F"
-        echo "Fragen aus der Datei [$(basename "$F" .md)](./$F)."  >> Fragenkatalog.md
+        echo "Fragen aus der Datei [$(basename "$F" .md)](./$(echo "$F" | sed -e 's/(/%28/g' -e 's/)/%29/g'))."  >> Fragenkatalog.md
         cat "$F" | sed -e '0,/#/s/^##*.*/<details><summary>&<\/summary>\n/' -e  's/^##*.*/<\/details>\n<details><summary>&<\/summary>\n/' -e 's/<summary>#[ ]*/<summary>/g' -e '$a</details>\n' >> Fragenkatalog.md
     done
 done
