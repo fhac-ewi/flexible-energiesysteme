78af34ceb41fa399870d9485c338a53b3b9a1190
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-02-19 19:50:54 +0000

    fixed sorting now?

--- create.sh
+++ create.sh
@@ -12,7 +12,7 @@ do
     echo "$D" | sed -e 's/:$//' -e 's/[^\/]*\/[0-9]*/#/g' -e 's/^/#/' >> Fragenkatalog.md # TODO Space nach Überschrift einfügen -e 's/^##*\([ ]*\)/ /1' 
     
     # for every file F in folder D
-    find "$D" -name "*.md" -maxdepth 1 -type f | sort | while read -r F
+    find "$D" -name "*.md" -maxdepth 1 -type f | sort -z | while read -r F
     do
         echo "Appending file $F"
         echo "Fragen aus der Datei [$(basename "$F" .md)](./$(echo "$F" | sed -e 's/(/%28/g' -e 's/)/%29/g'))."  >> Fragenkatalog.md
